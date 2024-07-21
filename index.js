3addd6a70d065ffc84735c53bcf19fcc93ca98eb
Boris Krasko <boris.krasko@live.com>
2024-07-21 12:39:04 +0200

    feat: add submitForm

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const submitForm = (e, type, title, html, dateFrom, timeFrom, dateTo, timeTo, companyId, state) => {
+  e.preventDefault();
+  console.log(`
+type: ${type.value}
+title: ${title.value}
+html: ${html.value}
+dateFrom: ${dateFrom.value}
+timeFrom: ${timeFrom.value}
+dateTo: ${dateTo.value}
+timeTo: ${timeTo.value}
+companyId: ${companyId.value}
+state: ${state.value}
+  `);
+};
+const copySQL = () => {
+  console.log('Copy SQL')
+};
+
 document.addEventListener('keyup', e => {
   switch (e.target.id) {
     case 'html':
@@ -11,6 +29,17 @@ document.addEventListener('keyup', e => {
       break;
     case 'modalTitle':
       title.value = modalTitle.textContent;
+      break;
+  }
+});
+
+document.addEventListener('click', e => {
+  switch (e.target.id) {
+    case 'submit':
+      submitForm(e, type, title, html, dateFrom, timeFrom, dateTo, timeTo, companyId, state);
+      break;
+    case 'copySQL': 
+      copySQL();
       break;
   }
 });
